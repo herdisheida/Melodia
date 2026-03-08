e029fb1eb68f5fc97a1eb40502d92318e7f700ad
Herdis Heida <herdisj24@ru.is>
2026-03-08 22:15:49 +0000

    template player bar

--- my-app/components/layout/PlayerBar.tsx
+++ my-app/components/layout/PlayerBar.tsx
@@ -1,21 +1,29 @@
 "use client";
 
 import { usePlayer } from "@/context/PlayerContext";
+import styles from "./PlayerBar.module.css";
 
 export default function PlayerBar() {
   const { currentSong } = usePlayer();
 
-  if (!currentSong) {
-    return <div>No song selected</div>;
-  }
-
   return (
-    <div>
-      <img src={currentSong.artworkUrl100} alt={currentSong.trackName} />
-      <div>
-        <p>{currentSong.trackName}</p>
-        <p>{currentSong.artistName}</p>
-      </div>
+    <div className={styles.bar}>
+      {currentSong ? (
+        <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+          <img
+            src={currentSong.artworkUrl100}
+            alt={currentSong.trackName}
+            width={60}
+            height={60}
+          />
+          <div>
+            <div>{currentSong.trackName}</div>
+            <div>{currentSong.artistName}</div>
+          </div>
+        </div>
+      ) : (
+        <p>No song selected</p>
+      )}
     </div>
   );
 }
