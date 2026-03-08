a650599d56b760fb59c6b5ecf3b738b3cc2196c9
Herdis Heida <herdisj24@ru.is>
2026-03-08 22:01:09 +0000

    add modules.css for easier css

--- my-app/components/search/SongCard.tsx
+++ my-app/components/search/SongCard.tsx
@@ -1,3 +1,29 @@
-// Album Art (artworkUrl100)
-// Track Name (trackName)
-// Artist Name (artistName)
+"use client";
+
+import type { Song } from "@/lib/types";
+import { usePlayer } from "@/context/PlayerContext";
+import Link from "next/link";
+import styles from "@/components/search/SongCard.module.css";
+
+export default function SongCard({ song }: { song: Song }) {
+  const { setCurrentSong } = usePlayer();
+
+  return (
+    <div className={styles.card}>
+      <div key={song.trackId} className={styles.info}>
+        <Link href={`/album/${song.collectionId}`}>
+          <img src={song.artworkUrl100} alt={song.trackName} />
+        </Link>
+
+        <h3>{song.trackName}</h3>
+        <p>{song.artistName}</p>
+
+        <Link href={`/album/${song.collectionId}`}>{song.collectionName}</Link>
+      </div>
+
+      <button onClick={() => setCurrentSong(song)} className={styles.playBtn}>
+        Play
+      </button>
+    </div>
+  );
+}
