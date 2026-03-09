e0fe582410d861031fedbdfcf50bc47cbe5dd8e7
Herdis Heida <herdisj24@ru.is>
2026-03-09 00:25:24 +0000

    css template favourite
    ugly

--- my-app/app/favorites/page.tsx
+++ my-app/app/favorites/page.tsx
@@ -1,7 +1,9 @@
 // localhost:3000/favorites
+import styles from "./FavoritesPage.module.css";
+
 export default function FavoritesPage() {
   return (
-    <main>
+    <main className={styles.container}>
       <h1>Favorites</h1>
       <p>Here you can see your favorite songs.</p>
       {/* TODO implement this? -- held við þurfutm ekki */}
