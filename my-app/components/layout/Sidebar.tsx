a99e9d577ff08e2d395f1e0d9cbbe305272af23d
Herdis Heida <herdisj24@ru.is>
2026-03-09 00:01:42 +0000

    todo

--- my-app/components/layout/Sidebar.tsx
+++ my-app/components/layout/Sidebar.tsx
@@ -5,6 +5,7 @@ export default function Sidebar() {
   return (
     <nav className={styles.sidebar}>
       <h2>Melodia</h2>
+      {/* TODO make the home and favourite page work */}
       <Link href="/">Home</Link>
       <Link href="/favorites">Favorites</Link>
     </nav>
