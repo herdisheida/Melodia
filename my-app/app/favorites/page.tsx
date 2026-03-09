a15d5c1839c246a430da842852c248cb3d2d9348
Herdis Heida <herdisj24@ru.is>
2026-03-09 00:23:53 +0000

    fix sidebar connectino og css

--- my-app/app/favorites/page.tsx
+++ my-app/app/favorites/page.tsx
@@ -1,1 +1,10 @@
 // localhost:3000/favorites
+export default function FavoritesPage() {
+  return (
+    <main>
+      <h1>Favorites</h1>
+      <p>Here you can see your favorite songs.</p>
+      {/* TODO implement this? -- held við þurfutm ekki */}
+    </main>
+  );
+}
